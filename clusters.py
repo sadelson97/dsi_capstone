eaf07bec5af8dd2509de5822c1ab9d12b239604d
Sam Adelson <samadelson1@Sams-MacBook-Pro.local>
2018-08-08 13:55:17 -0500

    possible way to construct seasons into a way to make model

--- clusters.py
+++ clusters.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import normalize
 import clean as c
 
 
-def create_clusters(df1=None,df2=None,df3=None):
+def create_clusters(df1=None,df2=None,df3=None,n_clusters = 5):
     """
     input: previous 3 seasons already cleaned
 
@@ -15,14 +15,20 @@ def create_clusters(df1=None,df2=None,df3=None):
     """
 
     game_avgs1 = c.get_team_avgs(df1)
-    game_avgs2 = c.get_team_avgs(df2)
-    game_avgs3 = c.get_team_avgs(df3)
+    try:
+        game_avgs2 = c.get_team_avgs(df2)
+    except:
+        pass
+    try:
+        game_avgs3 = c.get_team_avgs(df3)
+    except:
+        pass
     teams = game_avgs1.index
-    s_comb=game_avgs3
+    s_comb=game_avgs1
     s_comb=s_comb.append(game_avgs2).append(game_avgs1)
     s_comb=s_comb.groupby('team').mean()
     s_comb_norm = s_comb.drop(['FT_made','3PT_made','FG_made'],axis=1)
     s_comb_norm=normalize(s_comb_norm)
-    km = KMeans(n_clusters=5,n_init=20)
+    km = KMeans(n_clusters=n_clusters,n_init=20)
     km.fit(s_comb_norm)
     return s_comb, km
