47194698e98af08385302d3a3efe9653a6b372ac
Sam Adelson <samadelson1@Sams-MacBook-Pro.local>
2018-08-15 14:08:33 -0500

    added in some comments

--- rolling_mean.py
+++ rolling_mean.py
@@ -10,9 +10,8 @@ def group_dataframe(df):
 
     output: dataframe grouped by days, game_id, team
     '''
-    games=df.groupby(['days_after_opener','game_id','team']).sum()
-    #games = games[games['Min']<250] #need to figure this out
-    games['+/-']/=5
+    games=df.groupby(['days_after_opener','game_id','team']).sum() #group so its ordered on date, then game id, then team
+    games['+/-']/=5 #+/- is 5 times too large because there are 5 players on the court at all times
     return games
 
 
@@ -25,7 +24,7 @@ def get_team_games(df,team):
     output: return the dataframe with only that team
     '''
 
-    team_df = df[df.index.get_level_values('team')==team]
+    team_df = df[df.index.get_level_values('team')==team] #only select games where team index equals team you are looking for
     return team_df
 
 
@@ -37,22 +36,23 @@ def rolling_means(team_df,games_back=7,needed_games=5):
 
     output: rolling mean
     '''
-    roll = team_df.rolling(games_back,needed_games).mean()
-    roll['PTS_scored']= team_df.groupby(['days_after_opener','game_id','team']).sum()['PTS']
-    diffs = roll['PTS']-roll['PTS_scored']
-    roll.drop('PTS_scored',axis=1,inplace=True)
-    diffs_even = diffs[::2].values
-    diffs_odd = diffs[1::2].values
+    roll = team_df.rolling(games_back,needed_games).mean() #rolling means
+    roll['PTS_scored']= team_df.groupby(['days_after_opener','game_id','team']).sum()['PTS'] #need to calc score for each team so i can calc difference of average and that
+    diffs = roll['PTS']-roll['PTS_scored']  #differences of average - actual
+    roll.drop('PTS_scored',axis=1,inplace=True) #only need this feature for previous calculation
+    #the order of the differences is wrong. need to switch even and odd places
+    diffs_even = diffs[::2].values #even indices
+    diffs_odd = diffs[1::2].values #odd indices
     fixed_diff=[]
     for i in range(len(diffs_odd)):
-        fixed_diff.append(diffs_odd[i])
-        fixed_diff.append(diffs_even[i])
-    fixed_diff=pd.Series(fixed_diff,index=diffs.index).rolling(games_back,needed_games).mean()
-    roll['DEF_PTS']=fixed_diff
-    overtime = team_df['Min']>250
-    roll=roll.shift(1)
+        fixed_diff.append(diffs_odd[i]) #make odd first
+        fixed_diff.append(diffs_even[i]) #and even second
+    fixed_diff=pd.Series(fixed_diff,index=diffs.index).rolling(games_back,needed_games).mean() #get rolling average of it
+    roll['DEF_PTS']=fixed_diff #add in feature
+    overtime = team_df['Min']>250 #make overtime so i can not train on these games
+    roll=roll.shift(1) #shift it so current game is not taken into account in rolling mean
     roll['overtime']=pd.Series(overtime)
-    roll.drop('Total_PTS',axis=1,inplace=True)
+    roll.drop('Total_PTS',axis=1,inplace=True) #dont want rolling average of Total points
     return roll
 
 
@@ -64,11 +64,11 @@ def all_team_rolling(df,games_back=7,games_needed=5):
 
     return: dictionary containing all rolling mean dataframes
     '''
-    games = group_dataframe(df)
+    games = group_dataframe(df) #group the dataframe
     team_roll={}
-    for team in games.index.get_level_values('team'):
-        t = get_team_games(games,team)
-        team_roll[team]=rolling_means(t,games_back,games_needed)
+    for team in games.index.get_level_values('team'): #get each team
+        t = get_team_games(games,team) #dataframe for each team
+        team_roll[team]=rolling_means(t,games_back,games_needed) #add to dictionary to make dataframe later
     return team_roll
 
 
@@ -81,14 +81,14 @@ def complete_rolling_means(df,games_back=7,games_needed=5):
     output: dataframe with games and rolling means for teams
     '''
 
-    team_roll = all_team_rolling(df,games_back,games_needed)
+    team_roll = all_team_rolling(df,games_back,games_needed) #do previous function
     frames = []
-    for team in team_roll:
+    for team in team_roll: #make the dataframe
         frames.append(team_roll[team])
     roll_means_df = pd.concat(frames)
     df_merged = roll_means_df.groupby([roll_means_df.index.get_level_values('days_after_opener'),
                         roll_means_df.index.get_level_values('game_id'),
-                        roll_means_df.index.get_level_values('team')]).sum()
-    scores_df=df[['game_id','team','Total_PTS','days_after_opener']]
-    scores_df = scores_df.groupby(['days_after_opener','game_id','team']).mean()
+                        roll_means_df.index.get_level_values('team')]).sum() #group it so it is in the right order
+    scores_df=df[['game_id','team','Total_PTS','days_after_opener']] 
+    scores_df = scores_df.groupby(['days_after_opener','game_id','team']).mean() #add scores to the feature matrix so I can train
     return df_merged.join(scores_df)
