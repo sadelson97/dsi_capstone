93b5ab2e438c815e67c4c434036257489d3cc4a3
Sam Adelson <samadelson1@Sams-MacBook-Pro.local>
2018-08-15 14:08:19 -0500

    added in some comments

--- clean.py
+++ clean.py
@@ -21,6 +21,7 @@ def create_hometeam(season):
     home_team=[]
     for group in list(grouped.groups.fromkeys(season['game_id']).keys()): #need the game_id in the same order as the dataframe
         values = grouped.groups[group].get_values()
+        #with how espn box scores work, the away team is listed first and hometeam second
         home_team.append([False if season.iloc[values]['team'].iloc[i] ==  season.iloc[values]['team'].iloc[0] else True for i in range(len(values))])
     home_team = [item for sublist in home_team for item in sublist] #need to flatten list of lists into just a single list
     season['home_team']=pd.Series(home_team)
@@ -34,7 +35,7 @@ def calc_days_from_opener(season):
     output: none
     """
     season['date']=pd.Series([parse(season['date'][i]) for i in range(len(season))])
-    season['days_after_opener'] = season['date']-season['date'].min()
+    season['days_after_opener'] = season['date']-season['date'].min() #get a time delta for the number of days after the opener
     season.drop('date',axis=1,inplace=True)
 
 def make_splits(season):
@@ -47,19 +48,19 @@ def make_splits(season):
     """
 
     season.fillna('0',inplace=True) #string so it can be split later
-    season['Min']=season['Min'].map(lambda x: '0' if x.find('DNP')!= -1 else x)
-    season['Min']=season['Min'].map(lambda x: '0' if x.find('Did')!= -1 else x)
+    season['Min']=season['Min'].map(lambda x: '0' if x.find('DNP')!= -1 else x) #some seasons have DNP, need to replace with 0
+    season['Min']=season['Min'].map(lambda x: '0' if x.find('Did')!= -1 else x) #others have Did not play, also need to replace it
 
     season['FT_made']=season['FT'].map(lambda x: x.split('-')[0])
-    season['FT_attempts']=season['FT'].map(lambda x: x.split('-')[-1]) #when filled na with '0'
+    season['FT_attempts']=season['FT'].map(lambda x: x.split('-')[-1]) #-1 because when filled na with '0' it is not 1
 
     season['3PT_made']=season['3PT'].map(lambda x: x.split('-')[0])
-    season['3PT_attempts']=season['3PT'].map(lambda x: x.split('-')[-1]) #when filled na with '0'
+    season['3PT_attempts']=season['3PT'].map(lambda x: x.split('-')[-1]) #-1 because when filled na with '0' it is not 1
 
     season['FG_made']=season['FG'].map(lambda x: x.split('-')[0])
-    season['FG_attempts']=season['FG'].map(lambda x: x.split('-')[-1]) #when filled na with '0'
+    season['FG_attempts']=season['FG'].map(lambda x: x.split('-')[-1]) #-1 because when filled na with '0' it is not 1
 
-    season.drop(['FT','FG','3PT'],axis=1,inplace=True)
+    season.drop(['FT','FG','3PT'],axis=1,inplace=True) #drop the string of makes - attempts
 
 def convert_to_int(season):
     """
@@ -74,7 +75,7 @@ def convert_to_int(season):
     season[['+/-','home_team','AST','BLK','DREB','Min','OREB','PF','PTS','REB','STL','TO','FT_made',
             'FT_attempts','3PT_made','3PT_attempts','FG_made','FG_attempts']]=season[['+/-','home_team',
             'AST','BLK','DREB','Min','OREB','PF','PTS','REB','STL','TO','FT_made',
-            'FT_attempts','3PT_made','3PT_attempts','FG_made','FG_attempts']].astype(int)
+            'FT_attempts','3PT_made','3PT_attempts','FG_made','FG_attempts']].astype(int) #convert to int
     return season
 
 def add_total_score(season):
@@ -89,7 +90,7 @@ def add_total_score(season):
     game_totals=pd.DataFrame(grouped_games_score.sum()['PTS'])
     game_totals['Total_PTS']=game_totals['PTS']  #These two lines are because name 'PTS' needs to be changed
     game_totals.drop('PTS',axis=1,inplace=True)  #
-    season=season.join(game_totals,on='game_id')
+    season=season.join(game_totals,on='game_id') #join totals to the right game id
     return season
 
 def get_team_avgs(season):
@@ -108,21 +109,7 @@ def get_team_avgs(season):
     game_avgs['Possessions']=game_avgs['FG_attempts']+game_avgs['TO'] #add estimate for possessions
     return game_avgs
 
-# def get_team_avgs_2012(season): #for 2011/2012 season (nba lock out)
-#     """
-#     input: dataframe of season
-#
-#     get teams average stats
-#
-#     output: dataframe containing average stats
-#     """
-#     grouped_teams = season.groupby('team')
-#     game_avgs=pd.DataFrame(grouped_teams.sum())
-#     game_avgs = game_avgs[['AST','BLK','DREB','OREB','PF','REB','STL','TO',
-#             'FT_made','FT_attempts','3PT_made','3PT_attempts','FG_made','FG_attempts']]/66
-#     game_avgs['AVG_score']=grouped_teams.sum()['PTS']/66
-#     game_avgs['Possessions']=game_avgs['FG_attempts']+game_avgs['TO'] #add estimate for possessions
-#     return game_avgs
+
 
 def get_avgs_home_vs_away(season):
     """
@@ -150,22 +137,6 @@ def get_avgs_home_vs_away(season):
     # grouped_join = joined_data.groupby(['game_id','team']).mean()
     return avgs_home,avgs_away #grouped_join
 
-# def get_avgs_home_vs_away_2012(season):
-#     """
-#     input: dataframe of season
-#
-#     get team average stats based on away vs home games
-#
-#     output: dataframe containing these stats
-#     """
-#     small_df=season[['game_id','team','home_team','Total_PTS']]
-#     grouped_teams_home = season.groupby(['team','home_team'])
-#     avgs = grouped_teams_home.sum()[['AST','BLK','DREB','OREB','PF','REB','STL','TO',
-#             'FT_made','FT_attempts','3PT_made','3PT_attempts','FG_made','FG_attempts']]/41
-#     avgs['AVG_score']=grouped_teams_home.sum()['PTS']/33
-#     joined_data = small_df.join(avgs,on=['team','home_team'])
-#     grouped_join = joined_data.groupby(['game_id','team']).mean()
-#     return grouped_join
 
 def change_teamname(season):
     """
@@ -176,8 +147,8 @@ def change_teamname(season):
     output: none
     """
 
-    season['team']=season['team'].map(lambda x: 'Thunder' if x == 'SuperSonics' else x)
-    season['team']=season['team'].map(lambda x: 'Hornets' if x == 'Bobcats' else x)
+    season['team']=season['team'].map(lambda x: 'Thunder' if x == 'SuperSonics' else x) #supersonics to Thunder
+    season['team']=season['team'].map(lambda x: 'Hornets' if x == 'Bobcats' else x) #bobcats to hornets
 
 def change_hornets_pelicans(season): #use for 2004-2013
     """
@@ -193,7 +164,7 @@ def change_hornets_pelicans(season): #use for 2004-2013
 
 
 
-def all_clean(season):
+def all_clean(season): #seasons 2014-2018
     """
     input: dataframe of season
 
@@ -201,16 +172,16 @@ def all_clean(season):
 
     output: cleaned dataframe
     """
-    change_pos_position(season)
-    change_teamname(season)
-    create_hometeam(season)
-    calc_days_from_opener(season)
-    make_splits(season)
-    season=convert_to_int(season)
-    season=add_total_score(season)
+    change_pos_position(season) #make sure position labeled correctly
+    change_teamname(season) #changes supersonics and bobcats
+    create_hometeam(season) #create hometeam feature
+    calc_days_from_opener(season) #create days feature
+    make_splits(season) #change the stats that are in for 'x-y' to two features with 'x' and 'y'
+    season=convert_to_int(season) #convert all to ints
+    season=add_total_score(season) #add total scores
     return season
 
-def all_clean_pels(season):
+def all_clean_pels(season): #seasons 2004-2013
     """
     input: dataframe of season
 
@@ -218,12 +189,12 @@ def all_clean_pels(season):
 
     output: cleaned dataframe
     """
-    change_pos_position(season)
-    change_hornets_pelicans(season)
-    change_teamname(season)
-    create_hometeam(season)
-    calc_days_from_opener(season)
-    make_splits(season)
-    season=convert_to_int(season)
-    season=add_total_score(season)
+    change_pos_position(season) #make sure position labeled correctly
+    change_hornets_pelicans(season) #when new orleans was the hornets, change to pelicans
+    change_teamname(season) #changes supersonics and bobcats
+    create_hometeam(season) #create hometeam feature
+    calc_days_from_opener(season) #create days feature
+    make_splits(season) #change the stats that are in for 'x-y' to two features with 'x' and 'y'
+    season=convert_to_int(season) #convert all to ints
+    season=add_total_score(season) #add total scores
     return season
